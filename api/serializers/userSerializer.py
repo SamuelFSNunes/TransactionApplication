f169be7acf0bf648b13caa7debf3aa863a910202
SamuelFSNunes <samucasantana2003@gmail.com>
2024-06-10 19:39:36 -0300

    ref: change ModelSerializer to only serializer class

--- api/serializers/userSerializer.py
+++ api/serializers/userSerializer.py
@@ -1,7 +1,17 @@
-from rest_framework import serializers
 from api.models import User
+from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
+class UserSerializer(serializers.Serializer):
+    id = serializers.CharField(allow_blank=True, required=False)
+    name = serializers.CharField(max_length=255)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        return User(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        return instance
