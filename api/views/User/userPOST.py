53b1e4858e7f6c00e991867d9cf750515083ca46
SamuelFSNunes <samucasantana2003@gmail.com>
2024-06-10 19:42:30 -0300

    feat: create CRUD views

---
+++ api/views/User/userPOST.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect, render
+from rest_framework.views import APIView
+from rest_framework import status
+from api.serializers.userSerializer import User, UserSerializer
+from api.forms.userForm import UserForm
+from api.repository.repository import Repository
+
+class UserPOST(APIView):
+    repository = Repository(User, 'users')
+
+    def get(self, request):
+        userform = UserForm()
+        return render(request, "user/userForm.html", {"form": userform})
+    
+    def post(self, request):
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            serializer = UserSerializer(data=userform.cleaned_data)
+            if serializer.is_valid():
+                self.repository.create(serializer.validated_data)
+            else:
+                print(serializer.errors)
+        else:
+            print(userform.errors)
+
+        return redirect('users')
---
+++ api/views/__init__.py
@@ -0,0 +1,5 @@
+from .TetsConnection.testConnectionView import TestConnectionView
+from .User.userView import UserView
+from .User.userPOST import UserPOST
+from .User.userEDIT import UserEDIT
+from .User.userDELETE import UserDELETE
