a610e8a64221f58e414247d9bdaf09e39d3c7cb9
SamuelFSNunes <samucasantana2003@gmail.com>
2024-06-10 19:38:53 -0300

    ref: refactor repository without mongoengine

--- api/repository/repository.py
+++ api/repository/repository.py
@@ -1,29 +1,41 @@
-from mongoengine import Document
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class Repository:
-    def __init__(self, model: Document):
+    def __init__(self, model, collection):
         self.model = model
+        self.client = MongoClient('localhost:27017')
+        self.db = self.client['mongodatabase']
+        self.collection = self.db[collection]
 
     def get_all(self):
-        return list(self.model.objects.all())
+        documents = self.collection.find()
+        return [self.model.from_dict(doc) for doc in documents]
 
     def get_by_id(self, object_id):
-        return self.model.objects(id=object_id).first()
+        document = self.collection.find_one({"_id": ObjectId(object_id)})
+        if document:
+            return self.model.from_dict(document)
+        return None
 
     def create(self, data):
         obj = self.model(**data)
-        obj.save()
-        return obj
+        obj_dict = obj.to_dict()
+        result = self.collection.insert_one(obj_dict)
+        obj_dict["id"] = result.inserted_id
+        return self.model.from_dict(obj_dict)
 
     def update(self, object_id, data):
-        obj = self.model.objects(id=object_id).first()
-        if obj:
-            obj.update(**data)
-            obj.reload()
-        return obj
+        obj_dict = {k: v for k, v in data.items() if v is not None}
+        result = self.collection.update_one(
+            {"_id": ObjectId(object_id)},
+            {"$set": obj_dict}
+        )
+        if result.matched_count:
+            document = self.collection.find_one({"_id": ObjectId(object_id)})
+            return self.model.from_dict(document)
+        return None
 
     def delete(self, object_id):
-        obj = self.model.objects(id=object_id).first()
-        if obj:
-            obj.delete()
-        return obj
+        result = self.collection.delete_one({"_id": ObjectId(object_id)})
+        return result.deleted_count > 0
