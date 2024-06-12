fa13daceed2ed799222fd9975610c4b598244a45
SamuelFSNunes <samucasantana2003@gmail.com>
2024-06-12 13:19:11 -0300

    feat:  category urls

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import TestConnectionView, UserView, UserPOST, UserEDIT, UserDELETE
+from api.views import UserView, UserPOST, UserEDIT, UserDELETE
+from api.views import CategoryView, CategoryPOST, CategoryEDIT, categoryDELETE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Users
     path('api/users/', UserView.as_view(), name="users"), 
-    path('api/user/<id>', UserView.as_view(), name="user_detail"), 
     path('api/user/add', UserPOST.as_view(), name="create-user"),
+    path('api/user/detail/<id>', UserView.as_view(), name="user_detail"), 
     path('api/user/edit/<id>', UserEDIT.as_view(), name="edit-user"),
     path('api/user/delete/<id>', UserDELETE.as_view(), name="delete-user"),
+
+    #Categories
+    path('api/categories/', CategoryView.as_view(), name="categories"),
+    path('api/category/add', CategoryPOST.as_view(), name="create-category"),
+    path('api/category/detail/<id>', CategoryView.as_view(), name="category-detail"),
+    path('api/category/edit/<id>', CategoryEDIT.as_view(), name="edit-category"),
+    path('api/category/delete/<id>', categoryDELETE.as_view(), name="delete-category"),
 ]
