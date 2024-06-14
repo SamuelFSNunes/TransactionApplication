26546354003fef39e498d0764cb18ef1ef764500
SamuelFSNunes <samucasantana2003@gmail.com>
2024-06-14 19:42:38 -0300

    feat: implement acces control in transactions views

--- api/views/transaction/transactionEDIT.py
+++ api/views/transaction/transactionEDIT.py
@@ -4,11 +4,30 @@ from rest_framework import status
 from api.serializers.transactionSerializer import Transaction, TransactionSerializer
 from api.repository.repository import Repository
 from api.forms.transactionForm import TransactionForm
+from typing import Any
+from django.http import HttpRequest
+from api.auth.authentication import *
 
 class TransactionEDIT(APIView):
     repository = Repository(Transaction, 'transactions')
 
+    authenticate = False
+    user = None
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any):
+        cookie_token = request.COOKIES.get("auth_token", "Cookie not found")
+        error_code, _ = verifyToken(cookie_token)
+        print(error_code)
+
+        if error_code == 0:
+            user = getAuthenticatedUser(cookie_token)
+            self.authenticate = True
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request, id):
+        if not self.authenticate:
+            return redirect("login")
         if id:
             transaction = self.repository.get_by_id(id)
             if transaction:
@@ -20,6 +39,8 @@ class TransactionEDIT(APIView):
             return render(request, "transaction/transactionForm.html", {"form": transactionForm, 'errors': 'ID is required for edition'}, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, id):
+        if not self.authenticate:
+            return redirect("login")
         transactionForm = TransactionForm(request.POST)
         if transactionForm.is_valid():
             serializer = TransactionSerializer(data=transactionForm.cleaned_data)
