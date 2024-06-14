ced78af8de60fd06431894afccd9466c4686eacf
SamuelFSNunes <samucasantana2003@gmail.com>
2024-06-14 19:45:02 -0300

    feat: implement acces control in users views

--- api/views/User/userEDIT.py
+++ api/views/User/userEDIT.py
@@ -4,16 +4,34 @@ from rest_framework import status
 from api.serializers.userSerializer import User, UserSerializer
 from api.forms.userForm import UserForm
 from api.repository.repository import Repository
+from typing import Any
+from django.http import HttpRequest
+from api.auth.authentication import *
 
 class UserEDIT(APIView):
     repository = Repository(User, 'users')
 
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any):
+        cookie_token = request.COOKIES.get("auth_token", "Cookie not found")
+        error_code, _ = verifyToken(cookie_token)
+        print(error_code)
+
+        if error_code == 0:
+            user = getAuthenticatedUser(cookie_token)
+            self.authenticate = True
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request, id):
+        if not self.authenticate:
+            return redirect("login")
         user = self.repository.get_by_id(id)
         userform = UserForm(initial=user.to_dict())
         return render(request, "user/userFormEdit.html", {"form": userform, "id": id})
     
     def post(self, request, id):
+        if not self.authenticate:
+            return redirect("login")
         userform = UserForm(request.POST)
         if userform.is_valid():
             serializer = UserSerializer(data=userform.cleaned_data)
--- api/views/User/userPOST.py
+++ api/views/User/userPOST.py
@@ -4,15 +4,36 @@ from rest_framework import status
 from api.serializers.userSerializer import User, UserSerializer
 from api.forms.userForm import UserForm
 from api.repository.repository import Repository
+from typing import Any
+from django.http import HttpRequest
+from api.auth.authentication import *
 
 class UserPOST(APIView):
     repository = Repository(User, 'users')
 
+    authenticate = False
+    user = None
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any):
+        cookie_token = request.COOKIES.get("auth_token", "Cookie not found")
+        error_code, _ = verifyToken(cookie_token)
+        print(error_code)
+
+        if error_code == 0:
+            user = getAuthenticatedUser(cookie_token)
+            self.authenticate = True
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
+        if not self.authenticate:
+            return redirect("login")
         userform = UserForm()
         return render(request, "user/userForm.html", {"form": userform})
     
     def post(self, request):
+        if not self.authenticate:
+            return redirect("login")
         userform = UserForm(request.POST)
         if userform.is_valid():
             serializer = UserSerializer(data=userform.cleaned_data)
